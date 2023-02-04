3a72c08ad68130e168b4f12e75c0b6d8646c0adf
Ziad Ayman <Ziad@gmail.com>
2023-02-03 22:30:01 -0800

    Create home repo methods and failure file

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,10 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/features/home/data/models/models.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo
 {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  //Either I can Identify two data type, the value in the left side is error, the value in the right is correct or success
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
